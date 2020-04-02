f4fb35fd11947a8fa5188b232d4043b7fb255c95
xinanu <61746136+xinanu@users.noreply.github.com>
2020-04-02 16:41:50 +0200

    yes

---
+++ main.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Log in</h1>
+<form action="form_login.php" method="post">
+    <input type="text" name="username" placeholder="Enter Username">
+    <input type="password"  name="password" placeholder="Enter Password"><br>
+    <input type="submit" name="submit">
+</form>
+
+
+<h1>Register</h1>
+<form action="form_register.php" method="post">
+    <input type="text" name="username" placeholder="Enter Username">
+    <input type="password"  name="password" placeholder="Enter Password"><br>
+    <input type="submit" name="submit">
+</form>
+
+
+<br>
+<h1> <a href="form_users.php">Show users</a> </h1>
+
+<br>
+<h1> <a href="products.php">Show products</a> </h1>
+
+
+</body>
+</html>
